d9ae2ea8ee2eb221295e71c5694352d826b3c743
shakeRocketAndRoll <57106379+shakeRocketAndRoll@users.noreply.github.com>
2020-11-13 21:20:56 +0400

    second commit

--- src/main/AirportTest.java
+++ src/main/AirportTest.java
@@ -9,7 +9,16 @@ public class AirportTest {
         PlaneService service = new PlaneService();
         service.printAllInfo(plane1);
         Plane plane2 = new Plane("AM300", "Russia", 2006, 444, false, 130000, 35, 54, 120, 640000);
+        Plane plane3 = new Plane("Am310", "USA", 1994, 555, true, 124000, 33, 999, 300, 700000);
         service.printInfoSummary(plane1);
         service.newerPlane(plane1, plane2);
+        service.biggerWingspan(plane1, plane2);
+        service.smallestSeatNum(plane1, plane2, plane3);
+        Plane[] planes = {plane1, plane2, plane3};
+        service.printNonMilPlanes(planes);
+        service.printMilPlanes(planes);
+        service.minWeightPlane(planes);
+        service.minCostMilPlane(planes);
+        service.bubbleSortPlanes(planes);
     }
 }
